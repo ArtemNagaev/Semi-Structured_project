52e1a29cf3a35bcd953b15b098eddfa93e3822e9
ArtemNagaev <31846535+ArtemNagaev@users.noreply.github.com>
2020-12-25 17:28:03 +0500

    Update 4
    Добавлена возможность использовать иконки, статы (строки) и двойные статы.
    Добавил себя в Readme.

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from PyQt5 import Qt
 from PyQt5 import QtCore, QtGui, QtWidgets
 import json
 from pathlib import Path
-
+from PyQt5.QtGui import QPixmap 
 
 app = Qt.QApplication([])
 
@@ -13,59 +13,106 @@ data = json.loads(txt)
 print(txt)
 
 i = 0
-for item in data.values():
+
     
-    group = Qt.QGroupBox("item")
+for item in data.values():
+
+    #for key in item['stat']:
+    #    print(key)
+    
+    group = Qt.QGroupBox("Some item")
 
     
     VerticalLayout = Qt.QVBoxLayout(group)
 
-    sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.MinimumExpanding)
-    sizePolicy.setHorizontalStretch(0)
-    sizePolicy.setVerticalStretch(0)
+    sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.MinimumExpanding)
+    sizePolicy.setHorizontalStretch(1)
+    sizePolicy.setVerticalStretch(1)
     group.setSizePolicy(sizePolicy)
     
     layout.addWidget(group, i, 1)
 
-    print(item['name'])
-    #label = Qt.QLabel("tesesdasfasf")
-    #VerticalLayout.addWidget(label)
-    
+    #Название
     try:
         group.setTitle(item['name'])
         print(item['name'])
     except:
         print("no name")
-    try:
-        print(item['stat'])
-        label = Qt.QLabel(item['stat'])
-        VerticalLayout.addWidget(label)
-
-        i=i+1
-    except:
-        print("no stat")        
+        
+    #Иконка
     try:
         print(item['icon'])
-        label = Qt.QLabel(item['icon'])
+        # creating label 
+        label = Qt.QLabel("") 
+          
+        # loading image 
+        pixmap = QPixmap('images/'+item['icon']) 
+  
+        # adding image to label
+        
+        label.setPixmap(pixmap.scaled(200,200))
+        label.resize(50, 50)
         VerticalLayout.addWidget(label)
 
         i=i+1
     except:
         print("no icon")
-    print("\n")
-    i=i+1
 
-#for i in range(10):
-#    for j in range(5):
-#        label = Qt.QLabel("tesesdasfasf")
-#        layout.addWidget(label, i, j)
-        #button = Qt.QPushButton('{}x{}'.format(i, j))
-        #layout.addWidget(button, i, j)
+    #Один или массив статов
+    try:
+        print(item['stat'])
+        if(isinstance(item['stat'], list)):
+            for stat in item['stat']:
+                label = Qt.QLabel(stat)
+                VerticalLayout.addWidget(label)
+        else:
+            label = Qt.QLabel(item['stat'])
+            VerticalLayout.addWidget(label)
+        i=i+1
+    except:
+        print("no stat")
+    
+    #Один или массив двойных статов
+    print('statFloat')
+    #if(isinstance(item['statFloat'], list)):
+        #for statFloat in item['statFloat']:
+            #print(statFloat.get('statValue'))
+    
+    try:
+        if(isinstance(item['statFloat'], list)):
+            for statFloat in item['statFloat']:
+                group2 = Qt.QGroupBox("")
+                VerticalLayout2 = Qt.QHBoxLayout(group2)
+                label1 = Qt.QLabel(statFloat.get('statLabel')+": ")
+                label2 = Qt.QLabel(str(statFloat.get('statValue')))
+                VerticalLayout2.addWidget(label1)
+                VerticalLayout2.addWidget(label2)
+                VerticalLayout.addWidget(group2)
+                    
+            for statFloat in item['statFloat']:
+                print(statFloat['statLabel'])
+
+        else:
+            group2 = Qt.QGroupBox("")
+            VerticalLayout2 = Qt.QHBoxLayout(group2)
+            label1 = Qt.QLabel(item['statFloat'].get('statLabel')+": ")
+            label2 = Qt.QLabel(str(item['statFloat'].get('statValue')))
+            VerticalLayout2.addWidget(label1)
+            VerticalLayout2.addWidget(label2)
+            VerticalLayout.addWidget(group2)
+    except:
+        print("can't do statFloat")
+        
+    print("\n")
+    
+    i=i+1
+    
         
 w = Qt.QWidget()
 w.setLayout(layout)
 
 mw = Qt.QScrollArea()
+mw.resize(260, 900)
 mw.setWidget(w)
 mw.show()
 
